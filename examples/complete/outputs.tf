367814e1b0601de64419d82be5ee7f69e69cfb60
shanye <shanye.xzq@alibaba-inc.com>
2024-12-05 17:15:30 +0800

    examples: improve the examples

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,4 +1,4 @@
 output "this_vpn_pbr_route_entry_id" {
   description = "The PBR route entry ID of vpn."
-  value       = concat(alicloud_vpn_pbr_route_entry.vpn_pbr_route_entry[*].id, [""])[0]
+  value       = module.example.this_vpn_pbr_route_entry_id
 }
